31d7583696b92b0978d4c380f7539025f9d74cfb
sent-hil <me@sent-hil.com>
2012-10-15 14:54:28 -0400

    support double digit numbers

--- parser.c
+++ parser.c
@@ -5,29 +5,38 @@ typedef enum {
   NUMBER
 } object_type;
 
-
 /*define the object that will hold the parser stuff*/
 typedef struct object {
   object_type type;
-  int data;
+  long data;
 } object;
 
 /*write number to stdout*/
 object write(object obj) {
-  printf("[%d, %d]\n\n", obj.data-'0', obj.type);
+  printf("[%ld, %d]\n\n", obj.data, obj.type);
 }
 
 int main () {
   int input;
+  int input_index;
+  long num = 0;
+
   object input_object;
 
   while ((input = getc(stdin)) != EOF || input != '\n') {
     if (isdigit(input)) {
+      num = (num * 10) + (input - '0');
+    }
+
+    if (input == ' ' || input == '\n') {
       input_object.type = NUMBER;
-      input_object.data = input;
+      input_object.data = num;
 
       write(input_object);
+
+      num = 0;
     }
   }
+
   return 0;
 }
