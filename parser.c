93ef5705d518cef295b7df8261f0ff2028db5f4b
sent-hil <me@sent-hil.com>
2012-10-16 12:29:58 -0400

    support for strings

--- parser.c
+++ parser.c
@@ -23,6 +23,8 @@ typedef struct string_object {
   char data[1000];
 } string_object;
 
+int isdigit(int input);
+
 int isdelimiter(int input) {
   return input == EOF  ||
          input == '\n' ||
@@ -31,18 +33,52 @@ int isdelimiter(int input) {
          ;
 }
 
+int isstring(int input) {
+  return input == '"';
+}
+
 int main () {
   int input;
+  int next_input;
+  int string_index = 0;
+  int index = 0;
+  int string_open = 0;
   long num = 0;
+
+  number_object number_obj;
+  string_object string_obj;
 
   while ((input = getc(stdin)) != EOF) {
     if (isdelimiter(input) && num > 0) {
-      printf("%ld\n", num);
       num = 0;
+      printf("%ld\n", number_obj.data);
+
+      for (index = 0; index < string_index; index++) {
+        printf("%c\n", string_obj.data[index]);
+      }
+
+      string_index = 0;
     }
 
     if (isdigit(input)) {
       num = (num * 10) + (input - '0');
+      number_obj.data = num;
+    }
+
+    if (isstring(input)) {
+      printf("%c", string_obj.data[string_index]);
+
+      if ((next_input = getc(stdin)) == isstring(input)) {
+        string_index = 0;
+        string_open  = 0;
+        continue;
+      }
+      ungetc(input, stdin);
+
+      string_obj.data[string_index++] = input;
+      printf("", string_obj.data[string_index]);
     }
   }
+
+  return 0;
 }
