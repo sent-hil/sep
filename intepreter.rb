d46cffb4cbb0fd4a6a81298aee8cca9ad0f6b56d
sent-hil <me@sent-hil.com>
2012-10-25 15:42:18 -0400

    lambdas work

--- intepreter.rb
+++ intepreter.rb
@@ -1,37 +1,61 @@
 require 'pry'
 
-def reduc(operator)
-  Proc.new {|*args| args.reduce(&operator)}
-end
+#def reduc(operator)
+  #Proc.new {|*args| args.reduce(&operator)}
+#end
 
-GlobalEnviron = {
-  :+  => reduc(:+),
-  :-  => reduc(:-),
-  :*  => reduc(:*),
-  :/  => reduc(:/),
-  :%  => reduc(:%),
-  :>  => reduc(:>),
-  :<  => reduc(:<),
-  :== => reduc(:==)
-}
+#GlobalEnviron = [{
+  #:+   => 1,
+  #:-   => 2
+  #:+  => reduc(:+),
+  #:-  => reduc(:-),
+  #:*  => reduc(:*),
+  #:/  => reduc(:/),
+  #:%  => reduc(:%),
+  #:>  => reduc(:>),
+  #:<  => reduc(:<),
+  #:== => reduc(:==)
+#}]
 
-Environ = {}
+# loop to look up envs to find var
+#while env[0] || env[1]
+  #if env[0].has_key?(opt)
+    #break env[0][opt]
+  #else
+    #env = env[1]
+  #end
+#end
 
-class Intepreter
-  def eval(tokens, env)
+class Lisp
+  GlobalEnviron = [{
+    :+ => proc {|args| args.reduce(:+)}
+  }]
+
+  def evalulate(tokens, env=GlobalEnviron)
     if self_evaluating?(tokens[0])
       return tokens[0]
-    elsif is_definition?(tokens)
-      tokens.shift
-      tokens.flatten!
-      env[tokens[0]] = tokens[1]
+    elsif definition?(tokens)
+      env[0][tokens[1][0]] = tokens[1][1]
       return env
-    elsif variable?(tokens)
-      lookup_variable(tokens, env)
-    elsif tokens[0] == :lambda
-      tokens.shift
-      result = GlobalEnviron[tokens[1]].call(tokens[1])
-      return result
+    elsif lookup_definition?(tokens)
+      loop do
+        if env[0].has_key?(tokens[0])
+          break env[0][tokens[0]]
+        else
+          if env[1]
+            env = env[1]
+          else
+            break nil
+          end
+        end
+      end
+    elsif lumbda?(tokens)
+      function = evalulate(tokens[1])
+      results = tokens[2].map do |tok|
+        evalulate([tok])
+      end
+
+      return function.call(results)
     end
   end
 
@@ -43,19 +67,58 @@ class Intepreter
     false
   end
 
-  def is_definition?(tokens)
-    tokens[0] == :define
+  def definition?(tokens)
+    tokens[0] == :define && tokens[2].nil?
   end
 
-  def is_lambda(tokens)
-    tokens[0] == :lambda
+  def lookup_definition?(tokens)
+    tokens[0].is_a?(Symbol) && tokens.size == 1
   end
 
-  def variable?(tokens)
-    tokens[1].nil?
-  end
-
-  def lookup_variable(tokens, env)
-    env[tokens[0]]
+  def lumbda?(tokens)
+    tokens[0] == :lumbda
   end
 end
+
+#class Intepreter
+  #def eval(tokens, env)
+    #if self_evaluating?(tokens[0])
+      #return tokens[0]
+    #elsif is_definition?(tokens)
+      #tokens.shift
+      #tokens.flatten!
+      #env[tokens[0]] = tokens[1]
+      #return env
+    #elsif variable?(tokens)
+      #lookup_variable(tokens, env)
+    #elsif tokens[0] == :lambda
+      #tokens.shift
+      #result = GlobalEnviron[tokens[1]].call(tokens[1])
+      #return result
+    #end
+  #end
+
+  #def self_evaluating?(token)
+    #[Integer, String].each do |klass|
+      #return true if token.is_a?(klass)
+    #end
+
+    #false
+  #end
+
+  #def is_definition?(tokens)
+    #tokens[0] == :define
+  #end
+
+  #def is_lambda(tokens)
+    #tokens[0] == :lambda
+  #end
+
+  #def variable?(tokens)
+    #tokens[1].nil?
+  #end
+
+  #def lookup_variable(tokens, env)
+    #env[tokens[0]]
+  #end
+#end
--- intepreter_spec.rb
+++ intepreter_spec.rb
@@ -1,41 +1,129 @@
 require_relative 'intepreter'
 
-describe Intepreter do
+describe Lisp do
   subject { described_class.new }
-  before  { Environ.clear }
 
-  context '#self_evaluating?' do
-    it 'returns true for Integer' do
+  context 'type of token' do
+    it 'finds Integer' do
       subject.self_evaluating?(1).should == true
     end
 
-    it 'returns true for String' do
+    it 'finds String' do
       subject.self_evaluating?('Hello World').should == true
     end
 
-    it 'returns false for list' do
-      subject.self_evaluating?([]).should == false
+    it 'finds definition' do
+      subject.definition?([:define, [:x, 1]]).should == true
+    end
+
+    it 'finds lumbdas aka lambdas' do
+      subject.lumbda?([:lumbda, [:+], [1, 1]]).should == true
     end
   end
 
   it 'evals numbers' do
-    subject.eval([1], nil).should == 1
+    subject.evalulate([1]).should == 1
   end
 
   it 'evals strings' do
-    subject.eval(['Hello World'], nil).should == 'Hello World'
+    subject.evalulate(['Hello World']).should == 'Hello World'
   end
 
   it 'evals define variables' do
-    subject.eval([:define, [:x], [1]], Environ)[:x].should == 1
+    env = subject.evalulate([:define, [:x, 1]])
+    env[0][:x].should == 1
   end
 
-  it 'evals variables' do
-    Environ[:x] = 1
-    subject.eval([:x], Environ).should == 1
+  it 'evals define variables to current environ' do
+    env = [{}]
+    subject.evalulate([:define, [:x, 1]], env)
+    env[0][:x].should == 1
   end
 
-  it 'evals lambdas' do
-    subject.eval([:lambda, [:x], [:+, :x, :x]], Environ).should == ''
+  it 'evaling does not affect other environs' do
+    global = [{}]
+    env = [{}, global]
+    subject.evalulate([:define, [:x, 1]], env)
+    global[0].has_key?(:x).should == false
+  end
+
+  it 'eval lookup variables in current env' do
+    env = [{:x => 1}]
+    subject.evalulate([:x], env).should == 1
+  end
+
+  it 'eval lookup variables in one env up' do
+    global = [{}]
+    above_level = [{:x => 1}, global]
+    env = [{}, above_level]
+    subject.evalulate([:x], env).should == 1
+  end
+
+  it 'eval lookup variables in global env' do
+    global = [{:x => 1}]
+    env = [{}, global]
+    subject.evalulate([:x], env).should == 1
+  end
+
+  it 'returns nil for unknown variable' do
+    env = [{}]
+    subject.evalulate([:x], env).should == nil
+  end
+
+  it 'evals lumbda' do
+    env = [{:+ => proc {|args| args.reduce(:+)}}, {}]
+    subject.evalulate([:lumbda, [:+], [1, 1]], env).should == 2
+  end
+
+  it 'evals lumbda with vars' do
+    env = [
+      {
+        :- => proc {|args| args.reduce(:-)},
+        :x => 1
+    }, {}]
+
+    subject.evalulate([:lumbda, [:+], [:x, :x]], env).should == 2
   end
 end
+
+#describe Intepreter do
+  #subject { described_class.new }
+#
+#
+  #before  { Environ.clear }
+
+  #context '#self_evaluating?' do
+    #it 'returns true for Integer' do
+      #subject.self_evaluating?(1).should == true
+    #end
+
+    #it 'returns true for String' do
+      #subject.self_evaluating?('Hello World').should == true
+    #end
+
+    #it 'returns false for list' do
+      #subject.self_evaluating?([]).should == false
+    #end
+  #end
+
+  #it 'evals numbers' do
+    #subject.eval([1], nil).should == 1
+  #end
+
+  #it 'evals strings' do
+    #subject.eval(['Hello World'], nil).should == 'Hello World'
+  #end
+
+  #it 'evals define variables' do
+    #subject.eval([:define, [:x], [1]], Environ)[:x].should == 1
+  #end
+
+  #it 'evals variables' do
+    #Environ[:x] = 1
+    #subject.eval([:x], Environ).should == 1
+  #end
+
+  #it 'evals lambdas' do
+    #subject.eval([:lambda, [:x], [:+, :x, :x]], Environ).should == ''
+  #end
+#end
